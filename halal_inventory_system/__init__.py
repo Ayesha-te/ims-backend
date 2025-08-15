1edca1450d424af30eca168be9a392086cb08ad6
Ayesha Jahangir <ayeshajahangir280@gmail.com>
2025-08-15 16:28:22 +0500

    error

--- halal_inventory_system/__init__.py
+++ halal_inventory_system/__init__.py
@@ -1,2 +1,13 @@
 # Django project initialization
+from __future__ import absolute_import, unicode_literals
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+try:
+    from .celery import app as celery_app
+    __all__ = ('celery_app',)
+except ImportError:
+    # Celery is not required for basic functionality
+    pass
+
 __version__ = "1.0.0"
